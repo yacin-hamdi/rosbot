e710dda7d461b02dc650be7e69281624eb22926d
yacin <yacin.ha9@gmail.com>
2025-08-02 17:25:15 +0100

    create launch display file

---
+++ rosbot_description/launch/display.launch.py
@@ -0,0 +1,51 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from ament_index_python.packages import get_package_share_directory
+from launch_ros.parameter_descriptions import ParameterValue
+
+import os
+
+
+def generate_launch_description():
+    robot_description_dir = get_package_share_directory("rosbot_description")
+    robot_description_arg = DeclareLaunchArgument(
+        name="robot_description", 
+        default_value=os.path.join(robot_description_dir, "urdf", "rosbot.urdf.xacro")
+    )
+
+    robot_description = ParameterValue(Command(["xacro ", LaunchConfiguration("robot_description")]), value_type=str)
+
+    robot_state_publisher = Node(
+        package="robot_state_publisher", 
+        executable="robot_state_publisher", 
+        output="screen",
+        parameters=[
+            {"robot_description": robot_description}
+            ]
+    )
+
+    joint_state_publisher_gui = Node(
+        package="joint_state_publisher_gui", 
+        executable="joint_state_publisher_gui"
+    )
+
+    rviz2 = Node(
+        package="rviz2", 
+        executable="rviz2", 
+        name="rviz2", 
+        output="screen"
+    )
+
+
+
+
+
+
+    return LaunchDescription([
+        robot_description_arg, 
+        robot_state_publisher, 
+        joint_state_publisher_gui, 
+        rviz2
+    ])
