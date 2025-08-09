59e985bd021eb8129aad2cece69149f17799b229
yacin <yacin.ha9@gmail.com>
2025-08-09 19:11:49 +0100

    add laser scan link to urdf

--- rosbot_bringup/launch/bringup.launch.py
+++ rosbot_bringup/launch/bringup.launch.py
@@ -2,7 +2,6 @@ from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch_ros.substitutions import FindPackageShare
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import Node
 
 def generate_launch_description():
 
