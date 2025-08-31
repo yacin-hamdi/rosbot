592c9840ed133c36c754de88ec519e7149476aa0
yacin <yacin.ha9@gmail.com>
2025-08-31 17:31:02 +0100

    launch slam and amcl from bringup

--- rosbot_bringup/launch/bringup.launch.py
+++ rosbot_bringup/launch/bringup.launch.py
@@ -1,9 +1,18 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription, DeclareLaunchArgument
 from launch_ros.substitutions import FindPackageShare
-from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import PathJoinSubstitution, LaunchConfiguration
+from launch.conditions import IfCondition, UnlessCondition
+from launch_ros.actions import Node
 
 def generate_launch_description():
+
+    use_slam_arg = DeclareLaunchArgument(
+        name="use_slam", 
+        default_value="true"
+    )
+
+    use_slam = LaunchConfiguration("use_slam")
 
     gazebo = IncludeLaunchDescription(
         PathJoinSubstitution([
@@ -21,6 +30,7 @@ def generate_launch_description():
         ])
     )
 
+
     joystick = IncludeLaunchDescription(
         PathJoinSubstitution([
             FindPackageShare("rosbot_control"), 
@@ -32,20 +42,56 @@ def generate_launch_description():
         }.items()
     )
 
-    display = IncludeLaunchDescription(
-        PathJoinSubstitution([
-            FindPackageShare("rosbot_description"),
-            "launch", 
-            "display.launch.py"
-        ]),
-        launch_arguments={
-            "use_state_publisher": "False"
-        }.items()
+    # display = IncludeLaunchDescription(
+    #     PathJoinSubstitution([
+    #         FindPackageShare("rosbot_description"),
+    #         "launch", 
+    #         "display.launch.py"
+    #     ]),
+    #     launch_arguments={
+    #         "use_state_publisher": "False"
+    #     }.items()
+    # )
+
+    rviz2_slam = Node(
+        package="rviz2", 
+        executable="rviz2", 
+        name="rviz2", 
+        output="screen", 
+        arguments=[
+            "-d", 
+            PathJoinSubstitution([
+                FindPackageShare("rosbot_mapping"), 
+                "rviz", 
+                "slam.rviz"])
+            ],
+        condition=IfCondition(use_slam)
     )
 
+    slam = IncludeLaunchDescription(
+        PathJoinSubstitution([
+            FindPackageShare("rosbot_mapping"), 
+            "launch", 
+            "slam.launch.py"
+        ]), 
+        condition=IfCondition(use_slam)
+    )
+
+    amcl = IncludeLaunchDescription(
+        PathJoinSubstitution([
+            FindPackageShare("rosbot_localization"), 
+            "launch", 
+            "amcl.launch.py"
+        ]), 
+        condition=UnlessCondition(use_slam)
+    )
     return LaunchDescription([
+        use_slam_arg,
         gazebo,
         controller,
-        display,
-        joystick
+        rviz2_slam,
+        # display,
+        joystick, 
+        slam,
+        amcl
     ])
