fd104cb6d7a44d743f0b98d48602299965fa9dca
yacin <yacin.ha9@gmail.com>
2025-09-01 22:12:56 +0100

    add behavior tree navigator

--- rosbot_navigation/launch/navigation.launch.py
+++ rosbot_navigation/launch/navigation.launch.py
@@ -14,7 +14,7 @@ def generate_launch_description():
     
     use_sim_time = LaunchConfiguration("use_sim_time")
 
-    lifecycle_nodes = ["planner_server", "controller_server"]
+    lifecycle_nodes = ["planner_server", "controller_server", "bt_navigator"]
 
     nav2_lifecycle_manager = Node(
         package="nav2_lifecycle_manager", 
@@ -58,9 +58,25 @@ def generate_launch_description():
         ]
     )
 
+    bt_navigator = Node(
+        package="nav2_bt_navigator", 
+        executable="bt_navigator", 
+        name="bt_navigator", 
+        output="screen", 
+        parameters=[
+            PathJoinSubstitution([
+                FindPackageShare("rosbot_navigation"), 
+                "config", 
+                "bt_navigator.yaml"
+            ]), 
+            {"use_sim_time": use_sim_time}
+        ]
+    )
+
     return LaunchDescription([
         use_sim_time_arg, 
         planner_server, 
         controller_server,
+        bt_navigator,
         nav2_lifecycle_manager
     ])
